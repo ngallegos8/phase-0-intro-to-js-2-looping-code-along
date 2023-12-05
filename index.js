8f7add2f7a6173807ccda7d15e9345eef36e086c
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-05 11:27:28 -0700

    fixed final test to log 0 after 1, removed first console.log inside function countDown

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+/*
+for ([initialization]; [condition]; [iteration]) {
+  [loop body]
+}
+*/
+
+function writeCards(people) {
+    let thankYouMessages = []; // Create an empty array to store the thank you messages
+  
+    for (let p = 0; p < people.length; p++) {
+      thankYouMessages.push(`Thank you, ${people[p]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages; // Return the array of thank you messages
+  }
+
+  /*
+  function countDown(number) {
+    console.log("Number received:", number);
+    while (number > 0) {
+      console.log(number);
+      number -= 1;
+    }
+  }
+*/
+
+function countDown(number) {
+    /*console.log("Number received:", number);*/
+    while (number >= 0) {
+      console.log(number);
+      number -= 1;
+    };
+  }
