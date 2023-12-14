863fc1b99dfacc7bf585f9c416090bc1542a4b95
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-14 09:59:12 -0700

    3 passing tests | new functions

--- index.js
+++ index.js
@@ -1,3 +1,100 @@
+// for (let age = 30; age < 40; age++){
+//   console.log(`I'm ${age} years old. Happy birthday to me!`);
+//   debugger;
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// functon wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+// const person = (["Guadalupe", "Ollie", "Aki"])
+
+// function writeCards (person) {
+//   let message = [];
+//   for (let i = 0; i < person.length; i++) {
+//     writeCards.push(`Thank you, ${person[i]}, for the wonderful surprise gift!`);
+//   }
+//   return writeCards;
+// }
+
+ 
+function writeCards(people) {
+    let thankYouMessages = []; // Create an empty array to store the thank you messages
+  
+    for (let p = 0; p < people.length; p++) {
+      thankYouMessages.push(`Thank you, ${people[p]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages; // Return the array of thank you messages
+  }
+
+
+function countDown(place) {
+  while (place >= 0) {
+    console.log(place);
+    place -= 1;
+
+  }
+}
+
+
+
+
+// ORIGINAL WORKING !!!
+
+// function countDown(number) {
+//     /*console.log("Number received:", number);*/
+//     while (number >= 0) {
+//       console.log(number);
+//       number -= 1;
+//     };
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 const gifts = ["teddy bear", "drone", "doll"];
 
@@ -18,14 +115,19 @@ for ([initialization]; [condition]; [iteration]) {
 }
 */
 
-function writeCards(people) {
-    let thankYouMessages = []; // Create an empty array to store the thank you messages
+
+
+// ORIGINAL WORKING !!!
+// function writeCards(people) {
+//     let thankYouMessages = []; // Create an empty array to store the thank you messages
   
-    for (let p = 0; p < people.length; p++) {
-      thankYouMessages.push(`Thank you, ${people[p]}, for the wonderful surprise gift!`);
-    }
-    return thankYouMessages; // Return the array of thank you messages
-  }
+//     for (let p = 0; p < people.length; p++) {
+//       thankYouMessages.push(`Thank you, ${people[p]}, for the wonderful surprise gift!`);
+//     }
+//     return thankYouMessages; // Return the array of thank you messages
+//   }
+
+
 
   /*
   function countDown(number) {
@@ -37,10 +139,15 @@ function writeCards(people) {
   }
 */
 
-function countDown(number) {
-    /*console.log("Number received:", number);*/
-    while (number >= 0) {
-      console.log(number);
-      number -= 1;
-    };
-  }
+
+
+
+// ORIGINAL WORKING !!!
+
+// function countDown(number) {
+//     /*console.log("Number received:", number);*/
+//     while (number >= 0) {
+//       console.log(number);
+//       number -= 1;
+//     };
+//   }
